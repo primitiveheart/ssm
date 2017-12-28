80e3db9580d984b6140409f82495a90a80db90c1
primitiveheart <1132024350@qq.com>
2017-12-28 11:31:27 +0800

    first commit

---
+++ src/main/java/com/zgb/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.zgb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by admin on 2017/12/28.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "test";
+    }
+}
